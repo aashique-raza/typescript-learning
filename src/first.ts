ce8dd0988adbee011f99e7175426acca2f71a17f
farhan ansari <farhanraza2239@gmail.com>
2024-07-24 16:59:44 +0530

    end of day 1 practice

--- src/first.ts
+++ src/first.ts
@@ -1,1 +1,85 @@
 console.log('hiii this is first day')
+
+// interface someObj{
+//     value:string;
+//     id:number
+// }
+
+// const obj:someObj={
+//     value:'hiii',
+//     id:123
+// }
+
+// console.log(obj.value)
+
+
+// let value:string='hey what are you doing'
+
+// value='hey how are you'
+// // value=1233  
+
+// value=value.toLocaleUpperCase()
+// console.log(value)
+
+// let NumberValue:number=123456
+
+// NumberValue=23223324
+// // NumberValue='123434'
+
+// console.log(NumberValue.toLocaleString())
+
+
+// let isAwesome:boolean=true;
+// isAwesome=false
+// // isAwesome='ture'
+// isAwesome ? console.log('i am fine') : console.log('i am not fine')
+
+// union type----------------------------
+
+// let someValue:string | number;
+
+// someValue='true'
+// someValue=12344
+// someValue=true
+
+// let responseMessage:'success' | 'error' | 'pending'
+// responseMessage='let'
+// responseMessage='error'
+// responseMessage='404'
+
+// let books=['one man army','thief','sheldon','antomony']
+// // books=[12,3,4]
+
+// let foundBook:string | undefined ;
+
+// for(let book of books){
+//     if(book==='thies'){
+//         foundBook=book
+        
+//         break;
+//     }
+// }
+
+// console.log(foundBook.length)
+
+// console.log(foundBook)
+
+// console.log(foundBook?.length)
+// console.log(foundBook?.toLocaleLowerCase())
+
+
+
+const colors:string[]=['red','green','yellow','blue','pink']
+
+
+const findColorByName=(color:string,array:string[]):string | undefined =>{
+
+  for(let col of array){
+    if(col===color){
+let findColor:string | undefined =col
+return findColor
+    }
+  }
+}
+
+console.log(findColorByName('red',colors))
